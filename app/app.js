3b4f9725943eb624dd153add02cfd3a831104b99
dmeowmixer <dmeowmixer@hotmail.com>
2015-02-10 19:08:03 -1000

    Jenny is smart, she got the map to display Oahu, soon to come: make the island look like a map.

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
-var w=960,h=500,
-svg=d3.select("#chart")
-.append("svg")
-.attr("width",w)
-.attr("height",h);
+// var w=960,h=500,
+// svg=d3.select("#chart")
+// .append("svg")
+// .attr("width",w)
+// .attr("height",h);
  
-var text=svg
-.append("text")
-.text("hello world")
-.attr("y",50);
+// var text=svg
+// .append("text")
+// .text("hello world")
+// .attr("y",50);
+
+
+// d3.select('body').append('p');
+// d3.select('body').append('svg').attr('width', 50).attr('height', 50)
