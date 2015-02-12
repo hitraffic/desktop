7696f2baf724d21ca2394adf9b13787add3e49dd
dmeowmixer <dmeowmixer@hotmail.com>
2015-02-11 19:30:58 -1000

    deleted all subunits (was for kyle oba map) added code in app.js to display all incidents, skipped for loop iteration if longitude/ latitude is null.

--- app/app.js
+++ app/app.js
@@ -7,7 +7,20 @@ var map = L.map('map').setView([21.4678, -157.9807], 11);
       maxZoom: 18,
       }).addTo(map);
 
+// To add a css marker for something at long/lat
+// L.marker([21.4678, -157.9807], 11).addTo(map)
+//     .bindPopup('Some crazy shit happened here, avoid this area at all costs.')
+//     .openPopup();
 
-L.marker([21.4678, -157.9807], 11).addTo(map)
-    .bindPopup('Some crazy shit happened here, avoid this area at all costs.')
+
+for (var i = 0; i < incidents.length; i++){
+  if (incidents[i].lng === null || incidents[i].lat === null){
+    continue;
+  } 
+  var type = incidents[i].type;
+  var lng = incidents[i].lng;
+  var lat = incidents[i].lat;
+  L.marker([lat,lng],11).addTo(map)
+    .bindPopup(type)
     .openPopup();
+}
