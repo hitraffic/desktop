bd18d20c44ede0320d801535c4655dc64b34db07
TrinhNgoc <jennyngu@hawaii.edu>
2015-02-21 15:38:53 -1000

    Added heatmap and layer controls

--- app/app.js
+++ app/app.js
@@ -1,13 +1,56 @@
-var map = L.map('map').setView([21.4678, -157.9807], 11);
-    mapLink = 
+// var map = L.map('map').setView([21.4678, -157.9807], 11);
+var mapLink = 
       '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-    L.tileLayer(
+var baseLayer = L.tileLayer(
       'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; ' + mapLink,
       maxZoom: 18,
-      }).addTo(map);
+      });
 
+var testData = {
+  max: 5,
+  data: [{lat: 21.297355, lng:-157.861581, count: 3},{lat: 21.412146, lng:-157.746353, count: 2}]
+};
 
-L.marker([21.4678, -157.9807], 11).addTo(map)
+var cfg = {
+  // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+  // if scaleRadius is false it will be the constant radius used in pixels
+  "radius": 30,
+  "maxOpacity": 1, 
+  // scales the radius based on map zoom
+  "scaleRadius": false, 
+  // if set to false the heatmap uses the global maximum for colorization
+  // if activated: uses the data maximum within the current map boundaries 
+  //   (there will always be a red spot with useLocalExtremas true)
+  "useLocalExtrema": true,
+  // which field name in your data represents the latitude - default "lat"
+  latField: 'lat',
+  // which field name in your data represents the longitude - default "lng"
+  lngField: 'lng',
+  // which field name in your data represents the data value - default "value"
+  valueField: 'count'
+};
+
+var heatmapLayer = new HeatmapOverlay(cfg);
+
+var map = new L.Map('map', {
+  center: new L.LatLng(21.4678, -157.9807),
+  zoom: 11,
+  layers: [baseLayer, heatmapLayer]
+});
+
+heatmapLayer.setData(testData);
+
+var markers = L.marker([21.4678, -157.9807], 11).addTo(map)
     .bindPopup('Some crazy shit happened here, avoid this area at all costs.')
     .openPopup();
+
+var baseMaps = {
+  "Map" : baseLayer
+};
+var overlaysMaps = {
+  "Heatmap" : heatmapLayer,
+  "Popup" : markers
+};
+
+L.control.layers(baseMaps, overlaysMaps).addTo(map);
